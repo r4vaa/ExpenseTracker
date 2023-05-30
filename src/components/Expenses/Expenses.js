ce2630499b13ee31f6ef2a5d4d68d5e9cadb28f1
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-05-30 19:23:32 +0530

    conditional method 1 to display list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,9 +20,10 @@ const Expenses = (props) => {
       <div>
          <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.map((expense) => (
+            {filteredExpenses.length === 0 ? <p>no data found</p> : filteredExpenses.map((expense) => (
                <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
+            { }
          </Card>
       </div>
    )
