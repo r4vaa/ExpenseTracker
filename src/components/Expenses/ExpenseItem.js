65446924393cbf5f0bf0f3a941e6e88216bd389a
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-05-30 07:13:11 +0530

    using stateful list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,21 +6,21 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-	const [title, setTitle] = useState(props.title);
-	console.log('ExpenseItem evaluated by React')
+	// const [title, setTitle] = useState(props.title);
+	// console.log('ExpenseItem evaluated by React')
 
-	const clickedHandler = () => {
-		setTitle('Updated');
-		console.log(title)
-	}
+	// const clickedHandler = () => {
+	// 	setTitle('Updated');
+	// 	console.log(title)
+	// }
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
-			<button onClick={clickedHandler}>Change Title</button>
+			{/* <button onClick={clickedHandler}>Change Title</button> */}
 		</Card>
 
 	)
